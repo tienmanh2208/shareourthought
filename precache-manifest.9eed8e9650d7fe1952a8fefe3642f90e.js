4a601c4a054a0a160a549364c1c8c710d8ac3272
Tien Manh <nguyentienmanh6@gmail.com>
2020-03-21 13:55:31 +0700

    Add pages

---
+++ precache-manifest.9eed8e9650d7fe1952a8fefe3642f90e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d0e26bb7e37882a6f8aa06f92941f0",
+    "url": "/template-frontend/index.html"
+  },
+  {
+    "revision": "7848b9fd681c9b228786",
+    "url": "/template-frontend/static/css/main.b4199a92.chunk.css"
+  },
+  {
+    "revision": "697e189897ba23e784f1",
+    "url": "/template-frontend/static/js/2.7af62b43.chunk.js"
+  },
+  {
+    "revision": "6b094267100a51deec27dddafefb9fa1",
+    "url": "/template-frontend/static/js/2.7af62b43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7848b9fd681c9b228786",
+    "url": "/template-frontend/static/js/main.a97f0730.chunk.js"
+  },
+  {
+    "revision": "9458f39a47a6fbc77235",
+    "url": "/template-frontend/static/js/runtime-main.f8512e01.js"
+  }
+]);
