ccd19448532873af67125319c167d5b1d89920e5
Tien Manh <nguyentienmanh6@gmail.com>
2020-07-08 05:04:20 +0700

    2020-07-07

---
+++ precache-manifest.8d36bdc4f2287793226fd31b3252aa95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f57d8ebe8767c7cf592f40f739f394c",
+    "url": "/shareourthought/index.html"
+  },
+  {
+    "revision": "6e0b5d5b7787796ba636",
+    "url": "/shareourthought/static/css/main.85c92dc4.chunk.css"
+  },
+  {
+    "revision": "2f7aeb7ace909e01aebe",
+    "url": "/shareourthought/static/js/2.c7b043a3.chunk.js"
+  },
+  {
+    "revision": "17e5e2f3fc9d0a9d0829eedc200478c5",
+    "url": "/shareourthought/static/js/2.c7b043a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e0b5d5b7787796ba636",
+    "url": "/shareourthought/static/js/main.ec313b17.chunk.js"
+  },
+  {
+    "revision": "b2e9197460cadb9fda8a",
+    "url": "/shareourthought/static/js/runtime-main.cda0fedb.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/shareourthought/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/shareourthought/precache-manifest.4c2e5e5512d9f94d38d057db77b1ce77.js"
+  "/shareourthought/precache-manifest.8d36bdc4f2287793226fd31b3252aa95.js"
 );
 
 self.addEventListener('message', (event) => {
